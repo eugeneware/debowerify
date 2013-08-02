1847ba1b058147bad9db95d328fca9d6af03b4e0
Eugene Ware <eugene@noblesamurai.com>
2013-08-02 13:43:38 +1000

    Just test screenful for now.

--- test/index.js
+++ test/index.js
@@ -1,9 +1,7 @@
 var _screenfull = require('screenfull');
 var domready = require('domready');
-var testamd2 = require('testamd2');
 
 domready(function () {
-  testamd2();
   var button = document.getElementById('fullscreen');
   button.addEventListener('click', function (evt) {
     if (screenfull.enabled) {
