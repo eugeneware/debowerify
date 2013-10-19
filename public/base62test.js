9148c4d6ac552c734fb2132c21c83816d4e91b41
Devin Weaver <suki@tritarget.org>
2013-10-19 11:12:59 -0400

    Adds a spec for devDependencies

---
+++ public/base62test.js
@@ -0,0 +1,2 @@
+var Base62 = require('base62').base62;
+console.log(Base62.decode(Base62.encode(12345)));
--- test/index.js
+++ test/index.js
@@ -6,7 +6,8 @@ var expect = require('chai').expect,
     fs = require('fs');
 
 describe('debowerify', function() {
-  it('should be able to debowerify a basic file', function(done) {
+
+  it('should be able to debowerify a basic file from dependencies', function(done) {
     var jsPath = path.join(__dirname, '..', 'public', 'index.js');
     var b = browserify();
     b.add(jsPath);
@@ -23,4 +24,23 @@ describe('debowerify', function() {
       });
     });
   });
+
+  it('should be able to debowerify a basic file from devDependencies', function(done) {
+    var jsPath = path.join(__dirname, '..', 'public', 'base62test.js');
+    var b = browserify();
+    b.add(jsPath);
+    b.transform(debowerify);
+    b.bundle(function (err, src) {
+      if (err) return done(err);
+      vm.runInNewContext(src, {
+        console: {
+          log: function (msg) {
+            expect(msg).to.equal(12345);
+            done();
+          }
+        }
+      });
+    });
+  });
+
 });
